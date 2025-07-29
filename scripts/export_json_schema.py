9d9f49b3218a64fc474a2d3f3fe8665295556362
hoangson <lehoangsonsg@gmail.com>
2025-07-29 15:26:00 +0930

    feat: add README

--- scripts/export_json_schema.py
+++ scripts/export_json_schema.py
@@ -2,5 +2,5 @@ import json
 
 from appm.model import Template
 
-with open("../schema/yaml_template_schema.json", "w") as file:
+with open("schema/yaml_template_schema.json", "w") as file:
     json.dump(Template.model_json_schema(), file, indent=2)
